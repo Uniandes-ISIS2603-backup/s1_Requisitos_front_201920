d0f3824da75264d44bf6f29082afdce85d178831
Nicole <n.bahamon@uniandes.edu.co>
2019-12-04 13:42:07 -0500

    Sign up casi listo creando al back

--- src/app/auth/auth-sign-up/auth-sign-up.component.ts
+++ src/app/auth/auth-sign-up/auth-sign-up.component.ts
@@ -1,44 +1,112 @@
 import { Component, OnInit, Input, OnChanges, Output, EventEmitter } from '@angular/core';
-import { ToastrService } from 'ngx-toastr';
-
 import { AuthService } from '../auth.service';
 import { User } from '../user';
-
+import { ToastrService } from 'ngx-toastr';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Desarrollador } from '../../desarrollador/desarrollador';
+import { DesarrolladorService } from '../../desarrollador/desarrollador.service';
+import { DesarrolladorDetail } from '../../desarrollador/desarrollador-detail';
 @Component({
     selector: 'app-auth-sign-up',
     templateUrl: './auth-sign-up.component.html',
     styleUrls: ['./auth-sign-up.component.css']
 })
 export class AuthSignUpComponent implements OnInit {
-
+    
     /**
     * Constructor for the component
     * @param authService Auth service provider
     * @param toastrService The toastr to show messages to the user
     */
-    constructor(
-        private authService: AuthService,
-        private toastrService: ToastrService,
-    ) { }
+   constructor(
+    private authService: AuthService,
+    private toastrService: ToastrService,
+    private formBuilder: FormBuilder, 
+    private ds: DesarrolladorService
+) { }
 
-    user: User;
-
-    roles: String[];
-
+user: User;
+roles: String[];
+desarrolladorForm: FormGroup;
+detail: DesarrolladorDetail;
+desarrolladores:Desarrollador[];
+  
     /**
     * Sign the user up with the selected role
     */
     signUp(): void {
-        this.authService.login(this.user.role);
+        this.authService.login(this.detail.tipo);
         this.toastrService.success('Successfully signed up')
     }
-
+      
     /**
     * This function will initialize the component
     */
     ngOnInit() {
-        this.user = new User();
-        this.roles = ['Administrator', 'Client'];
-    }
-
+        this.desarrolladorForm = this.formBuilder.group({
+            nombre: ["", [Validators.required]],
+            correo: ["", [Validators.required]],
+            edad: ["", [Validators.required]],
+            cedula:["", [Validators.required]],
+            tipo:["", [Validators.required]], 
+          });
+    
+         this.user = new User();
+        this.roles = ['REPRESENTANTEDELCLIENTE', 'RESPONSABLE', 'COMUN'];
+        this.ds.getDesarrolladores().subscribe(c => (this.desarrolladores = c))
+       }
+       createDesarrollador(des: Desarrollador) 
+{
+        var id;
+        var that = this;
+        this.desarrolladores.forEach(function (value) {
+            if (value.nombre === that.desarrolladorForm.value.nombre) {
+                id = value.id;
+            }
+        });
+       
+        this.ds.createDesarrollador(des).subscribe((cas) => {
+            this.desarrolladores.push(cas);
+            this.toastrService.success("El desarrollador fue creado", "Desarrollador creado");
+        
+          }); 
+        this.ds.getDesarrolladorDetail(id)
+            .subscribe(DDetail => {
+                this.detail=DDetail;
+               localStorage.setItem('id', String(this.detail.id));
+                
+                this.toastrService.success("El desarrollador fue creado "+ DDetail.nombre + " Rol: "+DDetail.tipo , "si existe el usuario");
+            
+                this.signUp();
+            }, err => {
+              this.toastrService.error(err, "Error");
+            });
+           
+            
+       
+          
+          
+           
+          }
 }
+
+
+ 
+
+
+
+
+    
+
+    
+           
+
+
+
+
+
+
+  
+
+ 
+ 
