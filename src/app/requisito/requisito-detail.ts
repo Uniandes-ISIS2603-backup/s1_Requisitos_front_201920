9a4e267fc2a2cd2a06908272d605db4b0731813f
na.tobo <na.tobo@uniandes.edu.co>
2019-12-05 02:57:44 -0500

    Algunos cambios sobre list de requisitos y detalles

--- src/app/requisito/requisito-detail.ts
+++ src/app/requisito/requisito-detail.ts
@@ -1,6 +1,10 @@
 import {Requisito} from "./requisito";
+import {Casodeuso} from "../casodeuso/Casodeuso";
+import {Desarrollador} from "../desarrollador/desarrollador";
 
 export class RequisitoDetail extends Requisito
 {
   descripcion: string;
+  caso:Casodeuso;
+  desarrollador:Desarrollador;
 }
